3502ef53abf2a1f279967f754c502d794c4ca1bc
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-04-14 23:51:26 +0800

    Fixing bugs

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -6,72 +6,76 @@ import { cn, formatCurrency, formatPercentage } from '@/lib/utils';
 import DataTable from '../DataTable';
 import { fetcher } from '@/lib/actions/coingecko.actions';
 import { TrendingDown, TrendingUp } from 'lucide-react';
+import { CategoriesFallback } from './Fallback';
 
 const Categories = async () => {
-  const categories = await fetcher<Category[]>('coins/categories');
-
-  const columns: DataTableColumn<Category>[] = [
-    {
-      header: 'Category',
-      cellClassName: 'category-cell',
-      cell: (category) => category.name,
-    },
-    {
-      header: 'Top Gainers',
-      cellClassName: 'top-gainers-cell',
-      cell: (category) =>
-        category.top_3_coins.map((coin) => (
-          <Image src={coin} alt={coin} key={coin} width={20} height={20} />
-        )),
-    },
-    {
-      header: '24h Change',
-      cellClassName: 'change-cell',
-      cell: (category) => {
-        const isTrendingUp = category.market_cap_change_24h > 0;
-
-        return (
-          <div
-            className={cn(
-              'change-cell',
-              isTrendingUp ? 'text-green-500' : 'text-red-500'
-            )}
-          >
-            <p className="flex gap-2 items-center">
-              {formatPercentage(category.market_cap_change_24h)}
-              {isTrendingUp ? (
-                <TrendingUp width={16} height={16} />
-              ) : (
-                <TrendingDown width={16} height={16} />
-              )}
-            </p>
-          </div>
-        );
+  try {
+    const categories = await fetcher<Category[]>('coins/categories');
+    const columns: DataTableColumn<Category>[] = [
+      {
+        header: 'Category',
+        cellClassName: 'category-cell',
+        cell: (category) => category.name,
       },
-    },
-    {
-      header: 'Market Cap',
-      cellClassName: 'market-cap-cell',
-      cell: (category) => formatCurrency(category.market_cap),
-    },
-    {
-      header: '24th Volume',
-      cellClassName: 'volume-cell',
-      cell: (category) => formatCurrency(category.volume_24h),
-    },
-  ];
+      {
+        header: 'Top Gainers',
+        cellClassName: 'top-gainers-cell',
+        cell: (category) =>
+          category.top_3_coins.map((coin) => (
+            <Image src={coin} alt={coin} key={coin} width={20} height={20} />
+          )),
+      },
+      {
+        header: '24h Change',
+        cellClassName: 'change-cell',
+        cell: (category) => {
+          const isTrendingUp = category.market_cap_change_24h > 0;
 
-  return (
-    <div id="categories" className="custom-scrollbar">
-      <h4>Top Categories</h4>
+          return (
+            <div
+              className={cn(
+                'change-cell',
+                isTrendingUp ? 'text-green-500' : 'text-red-500'
+              )}
+            >
+              <p className="flex gap-2 items-center">
+                {formatPercentage(category.market_cap_change_24h)}
+                {isTrendingUp ? (
+                  <TrendingUp width={16} height={16} />
+                ) : (
+                  <TrendingDown width={16} height={16} />
+                )}
+              </p>
+            </div>
+          );
+        },
+      },
+      {
+        header: 'Market Cap',
+        cellClassName: 'market-cap-cell',
+        cell: (category) => formatCurrency(category.market_cap),
+      },
+      {
+        header: '24th Volume',
+        cellClassName: 'volume-cell',
+        cell: (category) => formatCurrency(category.volume_24h),
+      },
+    ];
 
-      <DataTable
-        columns={columns}
-        data={categories.slice(0, 10)}
-        rowKey={(category) => category.name}
-      />
-    </div>
-  );
+    return (
+      <div id="categories" className="custom-scrollbar">
+        <h4>Top Categories</h4>
+
+        <DataTable
+          columns={columns}
+          data={categories.slice(0, 10)}
+          rowKey={(category) => category.name}
+        />
+      </div>
+    );
+  } catch {
+    return <CategoriesFallback />;
+  }
 };
 
 export default Categories;
